dd6cf43a01e8b4ceb3ae79f2b476c3bf88f635b6
Shayan-Mst <shnbackup007@gmail.com>
2024-09-08 15:22:30 +0330

    added travel page

---
+++ src/app/travel/page.tsx
@@ -0,0 +1,147 @@
+"use client"
+import Footer from '@/components/Footer'
+import Sidebar from '@/components/Sidebar'
+import Topbar from '@/components/Topbar'
+import Navigation from '@/components/navigation'
+import React from 'react'
+
+const page = () => {
+  return (
+  <>
+  <Sidebar/>
+
+    <div>
+<Topbar/>
+
+<section className='xl:p-12 lg:p-4 md:p-8 sm:p-4 p-1'>
+<div className='bg-purple-950 sm:p-8 p-2 rounded-xl grid grid-cols-1 lg:grid-cols-2'>
+    <div className='text-white my-8'>
+<h2 className='sm:text-4xl text-3xl font-bold  my-5'>Travel Member Benefits</h2>
+
+<div className='flex my-3'>
+<svg className='mr-4' width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000" stroke-width="0.552"><g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> 
+<path fill-rule="evenodd" clip-rule="evenodd" d="M21.5821 5.54289C21.9726 5.93342 21.9726 6.56658 21.5821 6.95711L10.2526 18.2867C9.86452 18.6747 9.23627 18.6775 8.84475 18.293L2.29929 11.8644C1.90527 11.4774 1.89956 10.8443 2.28655 10.4503C2.67354 10.0562 3.30668 10.0505 3.70071 10.4375L9.53911 16.1717L20.1679 5.54289C20.5584 5.15237 21.1916 5.15237 21.5821 5.54289Z" fill="#ffffff"></path> </g></svg>
+Expert Travel Agent Services
+</div>
+<div className='flex my-3'>
+<svg className='mr-4' width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000" stroke-width="0.552"><g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> 
+<path fill-rule="evenodd" clip-rule="evenodd" d="M21.5821 5.54289C21.9726 5.93342 21.9726 6.56658 21.5821 6.95711L10.2526 18.2867C9.86452 18.6747 9.23627 18.6775 8.84475 18.293L2.29929 11.8644C1.90527 11.4774 1.89956 10.8443 2.28655 10.4503C2.67354 10.0562 3.30668 10.0505 3.70071 10.4375L9.53911 16.1717L20.1679 5.54289C20.5584 5.15237 21.1916 5.15237 21.5821 5.54289Z" fill="#ffffff"></path> </g></svg>
+
+Exclusive Extras Just for Members
+</div>
+<div className='flex my-3'>
+<svg className='mr-4' width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000" stroke-width="0.552"><g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> 
+<path fill-rule="evenodd" clip-rule="evenodd" d="M21.5821 5.54289C21.9726 5.93342 21.9726 6.56658 21.5821 6.95711L10.2526 18.2867C9.86452 18.6747 9.23627 18.6775 8.84475 18.293L2.29929 11.8644C1.90527 11.4774 1.89956 10.8443 2.28655 10.4503C2.67354 10.0562 3.30668 10.0505 3.70071 10.4375L9.53911 16.1717L20.1679 5.54289C20.5584 5.15237 21.1916 5.15237 21.5821 5.54289Z" fill="#ffffff"></path> </g></svg>
+
+Great Rates on Hotels and Rental Cars
+
+</div>
+<div className='flex my-3'>
+<svg className='mr-4' width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000" stroke-width="0.552"><g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+<g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> 
+<path fill-rule="evenodd" clip-rule="evenodd" d="M21.5821 5.54289C21.9726 5.93342 21.9726 6.56658 21.5821 6.95711L10.2526 18.2867C9.86452 18.6747 9.23627 18.6775 8.84475 18.293L2.29929 11.8644C1.90527 11.4774 1.89956 10.8443 2.28655 10.4503C2.67354 10.0562 3.30668 10.0505 3.70071 10.4375L9.53911 16.1717L20.1679 5.54289C20.5584 5.15237 21.1916 5.15237 21.5821 5.54289Z" fill="#ffffff"></path> </g></svg>
+
+Best Price Guarantee on Tour & Cruise Packages﻿*
+
+</div>
+</div>
+<div className='bg-gray-300 rounded-md px-4 py-12'>
+    <p className='text-center text-xl font-semibold mb-8'>What Can We Help You With?</p>
+<div className='grid grid-cols-3 sm:gap-8 lg:gap-4 gap-4'>
+
+<div className='bg-white rounded-md p-4 py-8 hover:opacity-80 cursor-pointer'>
+
+    <div>
+<svg className='mx-auto' width="30" height="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20.331 9.509h-2.012l-1.307-4.972a4.778 4.778 0 0 0-4.642-3.531H7.579a4.741 4.741 0 0 0-4.74 4l-1.8 11.672a3.059 3.059 0 0 0 .706 2.462A3.148 3.148 0 0 0 3.8 20.207a3.25 3.25 0 0 0 6.428.039H13.9a3.251 3.251 0 0 0 6.424 0 2.644 2.644 0 0 0 2.663-2.62v-5.5a2.644 2.644 0 0 0-2.656-2.617zm-9.843-7.473h1.882A3.747 3.747 0 0 1 16.015 4.8l1.238 4.71h-6.282a1.723 1.723 0 0 1-1.682-2.1zm-3.47 19.928a2.234 2.234 0 1 1 2.233-2.234 2.236 2.236 0 0 1-2.233 2.234zm10.1 0a2.234 2.234 0 1 1 2.233-2.234 2.236 2.236 0 0 1-2.237 2.234zm4.849-4.338a1.613 1.613 0 0 1-1.626 1.589 3.264 3.264 0 0 0-6.447 0h-3.653a3.324 3.324 0 0 0-3.223-2.749A3.324 3.324 0 0 0 3.8 19.185a2.12 2.12 0 0 1-1.269-.712 2.031 2.031 0 0 1-.471-1.634l1.8-11.672a3.715 3.715 0 0 1 3.719-3.131h1.853l-1.149 5.15a2.755 2.755 0 0 0 2.688 3.354h9.36a1.612 1.612 0 0 1 1.632 1.589z"/></svg>
+<p style={{color:"#003087"}} className='my-4 text-center'>Cars Search</p>
+</div>
+
+</div>
+
+<div className='bg-white rounded-md p-4 py-8 hover:opacity-80 cursor-pointer'>
+
+<div>
+<svg className='mx-auto' width="40" height="40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><path d="M50 38H37.94l1-2.65A1 1 0 0 0 38 34H27a1 1 0 0 0-1 .8l-.65 3.2h-1.46C16 38 6.8 34.3 6.05 26H19a1 1 0 0 0 0-2h-3.57l-2.83-9.18A4 4 0 0 0 8.78 12H5a1 1 0 0 0-1 1c0 12.45-.49 14.65 1.75 18.57C9.26 37.67 17.62 40 24.94 40l-.39 1.87a1 1 0 0 0 2 .4c1.45-7-.14.69 1.3-6.27h8.75l-8 21.34c-.34.88-.81.66-5.33.66l1.63-7.81a1 1 0 0 0-2-.4L21 58.8a1 1 0 0 0 1 1.2h5.61a3 3 0 0 0 2.82-2l1.89-5h2.18a3.49 3.49 0 0 0 .43-7l2.26-6H50a1 1 0 0 0 0-2zM6 14h2.78a2 2 0 0 1 1.91 1.41L13.34 24H6zm28.5 34a1.5 1.5 0 0 1 1.06 2.56c-.49.5-.86.44-2.49.44 1.29-3.45 1.01-3 1.43-3z"/><path d="M59 32.4a21.45 21.45 0 0 0-5.91-5.25c-5.9-3.6-10.82-3.15-15.9-3.15l-2.26-6a3.49 3.49 0 0 0-.43-7h-2.17l-.26-.69a1 1 0 0 0-1.88.7l4.87 13H27L23.23 6c5.27 0 4.93-.4 5.59 1.37a1 1 0 0 0 1.88-.7l-.28-.73A3 3 0 0 0 27.61 4c-6.07 0-6-.13-6.39.37s-.54-.78 3.72 19.63H23a1 1 0 0 0 0 2c20 0 21.77-.47 26.58 1.58A3.5 3.5 0 0 0 51.5 34h6.18a2.72 2.72 0 0 1-2.4 4H54a1 1 0 0 0 0 2h1.28A4.73 4.73 0 0 0 59 32.4zM34.5 13c1.85 0 2.26 3-.3 3l-1.13-3zm17 16c1 0 .71-.05 1.11.21A19.81 19.81 0 0 1 56 32h-4.5a1.5 1.5 0 0 1 0-3zM24.41 47.08A1.07 1.07 0 0 0 25 45a1.07 1.07 0 0 0-.59 2.08z"/><path d="M44 31v-2a1 1 0 0 0-2 0v2a1 1 0 0 0 2 0zM39 31v-2a1 1 0 0 0-2 0v2a1 1 0 0 0 2 0zM34 31v-2a1 1 0 0 0-2 0v2a1 1 0 0 0 2 0zM29 31v-2a1 1 0 0 0-2 0v2a1 1 0 0 0 2 0zM24 31v-2a1 1 0 0 0-2 0v2a1 1 0 0 0 2 0zM17 29v2a1 1 0 0 0 2 0v-2a1 1 0 0 0-2 0z"/></svg>   
+<p style={{color:"#003087"}} className='my-4 text-center'>Flights Search</p>
+</div>
+
+</div>
+
+<div className='bg-white rounded-md p-4 py-8 hover:opacity-80 cursor-pointer'>
+    <div>
+    <svg className='mx-auto' width="30" height="30" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M12 4h13.9a.09.09 0 0 1 .1.1V22a1 1 0 0 0 2 0V4.1A2.1 2.1 0 0 0 25.9 2H12a1 1 0 0 0 0 2zM29 28h-1v-2a1 1 0 0 0-2 0v2h-4v-5a1 1 0 0 0-1-1H11a1 1 0 0 0-1 1v5H6V4.1a.09.09 0 0 1 .1-.1H8a1 1 0 0 0 0-2H6.1A2.1 2.1 0 0 0 4 4.1V28H3a1 1 0 0 0 0 2h26a1 1 0 0 0 0-2zm-17-4h3v4h-3zm5 4v-4h3v4z"/><path d="M8 7a1 1 0 0 0 1 1h2a1 1 0 0 0 0-2H9a1 1 0 0 0-1 1zM17 6h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zM23 6h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zM11 10H9a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zM17 10h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zM23 10h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zM11 14H9a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zM17 14h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zM23 14h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zM11 18H9a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zM17 18h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zM23 18h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2z"/></svg>
+
+    <p style={{color:"#003087"}} className='my-4 text-center'>Hotels Search</p>
+
+    </div>
+</div>
+
+<div className='bg-white rounded-md p-4 py-8 hover:opacity-80 cursor-pointer'>
+
+<div>
+<svg className='mx-auto' width="40" height="40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><path d="M6 30v1a1 1 0 0 0 2 0v-1a1 1 0 0 0-2 0zM3.45 17.83 6 19.54V26a1 1 0 0 0 2 0c0-7.59.16-7.43-.45-7.83L5 16.46V14h10a1 1 0 0 0 0-2H4a1 1 0 0 0-1 1c0 4.3-.14 4.44.45 4.83zM27 21v4a1 1 0 0 0 2 0v-4a1 1 0 0 0-2 0zM33 21v4a1 1 0 0 0 2 0v-4a1 1 0 0 0-2 0zM21 21v4a1 1 0 0 0 2 0v-4a1 1 0 0 0-2 0zM22 33v4a1 1 0 0 0 2 0v-4a1 1 0 0 0-2 0zM28 33v4a1 1 0 0 0 2 0v-4a1 1 0 0 0-2 0zM56 41a1 1 0 0 0-2 0 1 1 0 0 0 2 0zM51 41a1 1 0 0 0-2 0 1 1 0 0 0 2 0zM46 41a1 1 0 0 0-2 0 1 1 0 0 0 2 0z"/><path d="M13 16h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zM13 20h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2zM13 24h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0-2z"/><path d="M62.8 36.4c-.41-.53-.54-.4-4.8-.4v-7a1 1 0 0 0-1-1H40V17a1 1 0 0 0-1-1H21v-3a1 1 0 0 0-1-1h-1a1 1 0 0 0 0 2v2.46c-2.7 1.8-3 1.84-3 2.54v21H8v-5a1 1 0 0 0-2 0v5H2a1 1 0 0 0-1 1c0 2.73-.39 5.43 2.56 6.9A2.63 2.63 0 0 1 5 50.24V55a1 1 0 0 0 1 1h10a1 1 0 0 0 0-2H7v-3.76a4.6 4.6 0 0 0-2.56-4.14C2.77 45.27 3 43.67 3 42h33c.47 0 .1.2 5.35-4H47a1 1 0 0 0 0-2v-3a1 1 0 0 0-2 0v3h-4v-3a1 1 0 0 0-2 0v4c0 .45.63-.18-3 2.72V30h20v6h-3v-3a1 1 0 0 0-2 0v3a1 1 0 0 0 0 2h9.68l-1.94 7H20a1 1 0 0 0 0 2h38.18a9 9 0 0 1-8.78 7H20a1 1 0 0 0 0 2h29.4A11 11 0 0 0 60 48c3.17-11.48 3.22-11 2.8-11.6zM33 28a1 1 0 0 0 0 2h1v10H18V30h11a1 1 0 0 0 0-2H18v-8.46L20.3 18H38v10z"/></svg>
+
+<p style={{color:"#003087"}} className='my-4 text-center'>Cruises Search</p>
+</div>
+
+
+</div>
+
+
+<div className='bg-white rounded-md p-4 py-8 hover:opacity-80 cursor-pointer'>
+
+    <div>
+
+    <svg className='mx-auto' xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 20.707 20">
+  <path d="M8.707 1h12v2h-12zM1.707 0h4v4h-4zM8.707 9h12v2h-12zM8.707 17h12v2h-12zM1.707 16h4v4h-4zM2.707 13.414l4.707-4.707L6 7.293l-3.293 3.293-1.293-1.293L0 10.707l2.707 2.707z"/>
+</svg>
+
+<p style={{color:"#003087"}} className='my-4 text-center'>Activities Search</p>
+
+ </div>
+</div>
+
+<div className='bg-white rounded-md p-4 py-8 hover:opacity-80 cursor-pointer'>
+
+
+<div>
+
+<svg className='mx-auto' width="40" height="40" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 62.2"><g data-name="Layer 2"><path d="m63.84 49.6-6.6-10.07 3.5-6.36c1.87-3.46.61-6.56-1.61-7.82a4.89 4.89 0 0 0-2-.57V19.2a7.34 7.34 0 0 0-7.33-7.33h-4.4V6.76A6.78 6.78 0 0 0 38.61 0H17.23a6.78 6.78 0 0 0-6.77 6.76v5.1H7.33A7.34 7.34 0 0 0 0 19.2v25.86a7.34 7.34 0 0 0 7.33 7.34h25.43a1 1 0 0 0 .76 1l5.25 1.23A3.64 3.64 0 0 0 40 59a2.58 2.58 0 0 0 .32.18l.28.16a3.66 3.66 0 0 0 4.35-1.23l3.8 3.81a1 1 0 0 0 .71.29h.13a1 1 0 0 0 .74-.49l2-3.5a1 1 0 0 0 0-1L49 52l2.88-4.28 7.84 7.62a1 1 0 0 0 1.57-.22l2.55-4.46a1 1 0 0 0 0-1.06zM12.48 13.87h7.91v3.31h-7.91zm8.91-2h-3.34V8.32a1 1 0 0 1 1-1h17.74a1 1 0 0 1 1 1v3.55h-16.4zm14.09 2h7.91v3.31h-7.91zM13.86 3.41A4.74 4.74 0 0 1 17.23 2h21.38a4.77 4.77 0 0 1 4.76 4.76v5.1h-3.55V8.32a3 3 0 0 0-3-3H19.07a3 3 0 0 0-3 3v3.55h-3.59V6.76a4.71 4.71 0 0 1 1.38-3.35zM7.33 13.87h3.14v4.31a1 1 0 0 0 1 1h9.91a1 1 0 0 0 1-1v-4.31h11.1v4.31a1 1 0 0 0 1 1h9.91a1 1 0 0 0 1-1v-4.31h4.39a5.34 5.34 0 0 1 5.33 5.33V25a6.47 6.47 0 0 0-3.5 3.07l-3.56 6.32-12-.31a1 1 0 0 0-.9.52L33 38.5H2V19.2a5.34 5.34 0 0 1 5.33-5.33zM2 45.06V40.5h31.14a1 1 0 0 0 .18.09l10.61 2.65-2.12 4.7-6.14-.06a1 1 0 0 0-.89.53l-1.06 2H7.33A5.34 5.34 0 0 1 2 45.06zM60.22 53l-7.77-7.56a1 1 0 0 0-1.53.16L47 51.4a1 1 0 0 0 0 1.09l3.31 5.23-1.08 1.85-3.73-3.73a1 1 0 0 0-.71-.29h-.1A1 1 0 0 0 44 56l-.17.27c-.45.8-1.51 1.68-2.3 1.29l-.13-.07-.3-.17c-.77-.48-.57-1.85-.1-2.71l.1-.22a1 1 0 0 0-.72-1.39l-5.14-1.2 1-1.89 6.19.06a1 1 0 0 0 .92-.59L46.24 43a1 1 0 0 0-.67-1.38L35.05 39l1.54-2.84 12 .31a1.07 1.07 0 0 0 .9-.51l3.85-6.83c1.38-2.41 3.45-2.72 4.79-2s2.15 2.69.83 5.12L55.2 39.1a1 1 0 0 0 0 1l6.59 10.05z" data-name="business trip"/></g></svg>
+
+<p style={{color:"#003087"}} className='my-4 text-center'>Trips Search</p>
+
+</div>
+
+</div>
+
+</div>
+</div>
+</div>
+<div className='my-16'>
+<p className='text-3xl font-semibold'>Featured Experiences and Deals</p>
+
+<div className='grid grid-cols-1 lg:grid-cols-2'>
+<div>
+
+</div>
+
+<div>
+
+    
+</div>
+
+
+</div>
+</div>
+</section>
+<Footer/>
+<Navigation/>
+    </div>
+    </>
+  )
+}
+
+export default page
